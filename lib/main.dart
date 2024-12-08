4d0a8a3ba73ee52c0582e6702828550033f81328
guilhermecostadesouza16 <guilhermecostadesouza16@gmail.com>
2024-12-08 19:18:04 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MinhaClasse());
+}
+
+class MinhaClasse extends StatelessWidget {
+  const MinhaClasse({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const MinhaPrimeiraPagina(titulo: 'Primeiro Programa'),
+    );
+  }
+}
+
+class MinhaPrimeiraPagina extends StatefulWidget {
+  const MinhaPrimeiraPagina({super.key, required this.titulo});
+
+  final String titulo;
+
+  @override
+  State<MinhaPrimeiraPagina> createState() => _MinhaPrimeiraPaginaState();
+}
+
+class _MinhaPrimeiraPaginaState extends State<MinhaPrimeiraPagina> {
+  int _contador = 0;
+
+  void _incrementarContador() {
+    setState(() {
+      _contador++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.titulo),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: _incrementarContador,
+          ),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Voce pressionou o botão este numero de vezes: $_contador'),
+            Text(
+              '$_contador',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementarContador,
+        tooltip: 'Incrementar',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
